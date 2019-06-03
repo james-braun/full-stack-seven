6e30e2a5aec882ee9bf33ce060c7c7f85a492a5d
james-braun <braun.james.robert@gmail.com>
2019-06-03 16:56:02 -0500

    addded spinner

--- src/components/Items.js
+++ src/components/Items.js
@@ -36,7 +36,22 @@ class Items extends PureComponent {
             this.performSearch(this.props.match.params.name);
             return (
                 <div className="photo-container">
-                    <h2>Loading...</h2>
+                    <h2>loading...</h2>
+                    {/*sk-fading-circle curtesy https://tobiasahlin.com/spinkit/ */}
+                    <div class="sk-fading-circle">
+                        <div class="sk-circle1 sk-circle"></div>
+                        <div class="sk-circle2 sk-circle"></div>
+                        <div class="sk-circle3 sk-circle"></div>
+                        <div class="sk-circle4 sk-circle"></div>
+                        <div class="sk-circle5 sk-circle"></div>
+                        <div class="sk-circle6 sk-circle"></div>
+                        <div class="sk-circle7 sk-circle"></div>
+                        <div class="sk-circle8 sk-circle"></div>
+                        <div class="sk-circle9 sk-circle"></div>
+                        <div class="sk-circle10 sk-circle"></div>
+                        <div class="sk-circle11 sk-circle"></div>
+                        <div class="sk-circle12 sk-circle"></div>
+                    </div>
                 </div>
             );
         } else {
